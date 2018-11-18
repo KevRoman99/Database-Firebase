20aca22136331973b872e81c9b5a186250bd8561
KevRoman99 <kevin.romane99@gmail.com>
2018-11-18 16:11:18 -0600

    Firebase configuracion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAk1ZaZvuvlGz5B9WE3JeQeHfgDcaFo6_0",
+    authDomain: "database-b6b06.firebaseapp.com",
+    databaseURL: "https://database-b6b06.firebaseio.com",
+    projectId: "database-b6b06",
+    storageBucket: "database-b6b06.appspot.com",
+    messagingSenderId: "848893838672"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAk1ZaZvuvlGz5B9WE3JeQeHfgDcaFo6_0",
+    authDomain: "database-b6b06.firebaseapp.com",
+    databaseURL: "https://database-b6b06.firebaseio.com",
+    projectId: "database-b6b06",
+    storageBucket: "database-b6b06.appspot.com",
+    messagingSenderId: "848893838672"
+  }
 };
 
 /*
